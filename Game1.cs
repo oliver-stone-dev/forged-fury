3ae2b6f20fecb002746e282d5e555f49469ceffd
oliver-stone-dev <oliver.stone.dev@outlook.com>
2025-01-20 11:48:02 +0000

    Added background and level

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.ComponentModel;
 
 namespace forged_fury
 {
@@ -8,6 +10,11 @@ namespace forged_fury
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D _windowBackground;
+        private Texture2D _levelBackground;
+        private const int _backgroundWidth = 448;
+        private const int _backgroundHeight = 288;
+        private const float _spriteScaleAmount = 1.4f;
 
         public Game1()
         {
@@ -26,8 +33,9 @@ namespace forged_fury
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            _levelBackground = Content.Load<Texture2D>("Level");
+            _windowBackground = new Texture2D(GraphicsDevice, 1, 1);
+            _windowBackground.SetData(new Color[] { Color.White });
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,6 +53,20 @@ namespace forged_fury
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_windowBackground, 
+                              new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight), 
+                              Color.Black);
+
+            int width = Convert.ToInt32(_backgroundWidth * _spriteScaleAmount);
+            int height = Convert.ToInt32(_backgroundHeight * _spriteScaleAmount);
+
+            _spriteBatch.Draw(_levelBackground,new Rectangle(_graphics.PreferredBackBufferWidth/2-(width / 2), 
+                                                             _graphics.PreferredBackBufferHeight/2 - (height / 2),
+                                                             width,
+                                                             height),
+                                                             Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
