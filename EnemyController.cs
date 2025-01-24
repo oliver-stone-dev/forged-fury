54a0eb325202ba81d54ab46b566dee8864b768ac
oliver-stone-dev <oliver.stone.dev@outlook.com>
2025-01-24 08:59:01 +0000

    Added attack collision to enemy

--- EnemyController.cs
+++ EnemyController.cs
@@ -15,7 +15,10 @@ public class EnemyController : Character
 
     private int _attackCooldownMs = 1000;
     private bool _hasAttackedFlag = false;
+    private bool _attacking = false;
     private int _attackCooldownTimer = 0;
+
+    private Collider _attackCollider;
 
     public static readonly float _defaultAttackDistance = 50f;
 
@@ -25,13 +28,33 @@ public class EnemyController : Character
 
     public EnemyController(Texture2D texture2D, Character playerToFollow) : base(texture2D)
     {
+        _attackCollider = new(this);
+        _attackCollider.Enabled = false;
+        _attackCollider.Height = 50;
+        _attackCollider.Width = 10;
+
         MinAttackDistance = _defaultAttackDistance;
         _playerToFollow = playerToFollow;
+        _characterCollider.OnCollisionAction = OnCharacterCollision;
+        _attackCollider.OnCollisionAction = OnAttackCollision;
     }
 
     public override void Update(GameTime gameTime)
     {
-        //FollowTarget();
+        if (_characterDirection == Character.Direction.Right)
+        {
+            var pos = Position;
+            pos.X += 40;
+            _attackCollider.Position = pos;
+        }
+        else
+        {
+            var pos = Position;
+            pos.X -= 40;
+            _attackCollider.Position = pos;
+        }
+
+        FollowTarget();
         ResetAttack(gameTime);
         base.Update(gameTime);
     }
@@ -59,10 +82,12 @@ public class EnemyController : Character
     private void Attack()
     {
         if (_hasAttackedFlag) return;
-        
+
+        _attacking = true;
         _attackFlag = true;
         _hasAttackedFlag = true;
         _attackCooldownTimer = _attackCooldownMs;
+        _attackCollider.Enabled = true;
     }
 
     private void ResetAttack(GameTime gameTime)
@@ -76,5 +101,23 @@ public class EnemyController : Character
             _attackFlag = false;
             _attackCooldownTimer = 0;
         }
+    }
+
+    private void OnCharacterCollision(Collider collider)
+    {
+        //collider.Parent.Destroy();
+        //Debug.WriteLine($"Character collision with {collider.Parent.Name}");
+    }
+
+    private void OnAttackCollision(Collider collider)
+    {
+        if (_attacking)
+        {
+            _attackCollider.Enabled = false;
+        }
+
+        _attacking = false;
+
+        //Debug.WriteLine($"Attack collision with {collider.Parent.Name}");
     }
 }
